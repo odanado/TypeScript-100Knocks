d590f5de4f41d98173218027045c8682fe746962
odan <odan3240@gmail.com>
2019-06-01 00:16:50 +0900

    add 01

---
+++ src/01.js
@@ -0,0 +1,8 @@
+export function sum() {
+  let result = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i]
+  }
+
+  return result
+}
--- test/00.ts
+++ test/00.ts
@@ -1,6 +1,8 @@
 import { assert, _ } from "spec.ts";
 import { add } from "../src/00"
 
+add(10, 32)
+
 type Params = Parameters<typeof add>;
 type Return = ReturnType<typeof add>;
 
